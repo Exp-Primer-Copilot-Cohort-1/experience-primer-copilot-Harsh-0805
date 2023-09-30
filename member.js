341e2a80c024ca25be3bf9e0aad32eee7786f407
Harsh-0805 <93078888+Harsh-0805@users.noreply.github.com>
2023-09-30 16:23:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberBtn = document.getElementById("member-btn");
+    var skillsBtn = document.getElementById("skills-btn");
+
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberBtn.style.color = "#fff";
+    skillsBtn.style.color = "#000";
+}
